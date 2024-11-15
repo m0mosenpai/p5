c7da34cf03e1239760022fba983cd66595bb8e26
Sarthak Khattar <sarthakoct@gmail.com>
2024-11-14 23:49:50 -0600

    added basic functionality for tasks 1 and 2

--- xv6-public/sysproc.c
+++ xv6-public/sysproc.c
@@ -6,6 +6,13 @@
 #include "memlayout.h"
 #include "mmu.h"
 #include "proc.h"
+#include "wmap.h"
+#include "vm.h"
+#include "stdint.h"
+#include <stdint.h>
+
+extern pte_t* walkpgdir(pde_t *pgdir, const void *va, int alloc);
+extern int mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm);
 
 int
 sys_fork(void)
@@ -92,20 +99,79 @@ sys_uptime(void)
 
 int
 sys_wmap(void) {
-    return 0;
+  uint addr;
+  int length;
+  int flags;
+  int fd;
+
+  if (argint(0, (int*)&addr) < 0 ||
+    argint(1, (int*)&length) < 0 ||
+    argint(2, (int*)&flags)  < 0 ||
+    argint(3, (int*)&fd)     < 0
+  ) return FAILED;
+
+  // length should be positive,
+  // flags should always be set to MAP_FIXED and MAP_SHARED
+  // fd should be valid if MAP_ANONYMOUS is not set
+  // addr should be valid and page divisible
+  if ((length <= 0)                                                ||
+    (flags & MAP_FIXED) != MAP_FIXED                               ||
+    (flags & MAP_SHARED) != MAP_SHARED                             ||
+    (flags & MAP_ANONYMOUS) != MAP_ANONYMOUS && fd < 0             ||
+    (addr % PGSIZE != 0 || (addr < 0x60000000 || addr > KERNBASE))
+  ) return FAILED;
+
+  // TO-DO: handle MAP_SHARED/ MAP_ANONYMOUS
+  // TO-DO: lazy mapping
+  int i;
+  for (i = 0; i < length / PGSIZE; i++) {
+    char *mem = kalloc();
+    if (mem == 0) return FAILED;
+    pde_t *pgdir = myproc()->pgdir;
+    mappages(pgdir, (void*)(uintptr_t)(addr + i*PGSIZE), PGSIZE, V2P((uintptr_t)mem), PTE_W | PTE_U);
+  }
+
+  return addr;
 }
 
 int
 sys_wunmap(void) {
-    return 0;
+  uint addr;
+
+  if (argint(0, (int*)&addr) < 0)
+    return FAILED;
+
+  // addr should be valid and page divisible
+  if (addr % PGSIZE != 0 || (addr < 0x60000000 || addr > KERNBASE))
+    return FAILED;
+
+  // TO-DO: loop and unmap all pages (when to end loop??)
+  // TO-DO: lazy unmapping
+  pde_t *pgdir = myproc()->pgdir;
+  pte_t *pte = walkpgdir(pgdir, (void*)(uintptr_t)addr, 0);
+  // addr should have a valid PTE
+  if (pte == 0) return FAILED;
+
+  uint phys_addr = PTE_ADDR(*pte);
+  kfree(P2V((uintptr_t)phys_addr));
+  *pte = 0;
+
+  return SUCCESS;
 }
 
 int
 sys_va2pa(void) {
-    return 0;
+  uint va;
+
+  // TO-DO: translate va->pa and return pa
+  if (argint(0, (int*)&va) < 0)
+    return FAILED;
+
+  return 0;
 }
 
 int
 sys_getwmapinfo(void) {
-    return 0;
+  // TO-DO: populate struct
+  return 0;
 }
