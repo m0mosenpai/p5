14c212b7e872133f37b934f9eba5ca71ec0ea98a
Sarthak Khattar <sarthakoct@gmail.com>
2024-11-16 20:04:33 -0600

    updated if case, added seg fault

--- xv6-public/trap.c
+++ xv6-public/trap.c
@@ -77,6 +77,10 @@ trap(struct trapframe *tf)
             cpuid(), tf->cs, tf->eip);
     lapiceoi();
     break;
+  case T_PGFLT:
+    cprintf("Segmentation Fault\n");
+    exit();
+    return;
 
   //PAGEBREAK: 13
   default:
